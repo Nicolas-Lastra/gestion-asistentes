4fea4446a14de90d5fbd9df19cd0701e290b7690
Nicolas Lastra <nicolas.lastra.inzunza@gmail.com>
2025-07-19 14:15:17 -0300

    Adding pagination for students list with Angular Material

--- src/app/students-table/students-table.ts
+++ src/app/students-table/students-table.ts
@@ -1,17 +1,30 @@
 import { Component, Input } from '@angular/core';
 import { Student } from '../../shared/entities';
-import {MatTableModule} from '@angular/material/table';
+import {MatTableDataSource, MatTableModule} from '@angular/material/table';
 import { FullnamePipe } from '../../shared/pipes/fullname-pipe';
+import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
+import { ViewChild, AfterViewInit } from '@angular/core';
+
 
 @Component({
   selector: 'app-students-table',
-  imports: [MatTableModule, FullnamePipe],
+  imports: [MatTableModule, FullnamePipe, MatPaginatorModule],
   templateUrl: './students-table.html',
   styleUrl: './students-table.css'
 })
-export class StudentsTable {
 
-  @Input() students: Student[] = [];
+export class StudentsTable implements AfterViewInit{
+
+  @Input() set students(value: Student[]) {
+    this.dataSource.data = value;
+  }
 
   displayedColumns: string[] = ['id', 'name', 'dni', 'email'];
+  dataSource = new MatTableDataSource<Student>();
+
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+  }
 }
