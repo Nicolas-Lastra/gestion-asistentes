e58d4c8fe07dc4d73c5903516881e55de9e7a749
Nicolas Lastra <nicolas.lastra.inzunza@gmail.com>
2025-08-04 12:13:20 -0300

    Refactoring the component to work with router-outlet

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,13 +1,19 @@
 import { Component } from '@angular/core';
-import { StudentsSection } from './students-section/students-section';
 import { Toolbar } from './toolbar/toolbar';
+import { Navbar } from "./navbar/navbar";
+import { RouterModule } from "@angular/router";
+import { HttpClient } from '@angular/common/http';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-root',
-  imports: [StudentsSection, Toolbar],
+  imports: [Toolbar, Navbar, RouterModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected title = 'gestion-asistentes';
+  activeSection = "studentsSection";
+
+  constructor(private http: HttpClient, private _snackBar: MatSnackBar) { }
 }
