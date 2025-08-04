4e8958f848fcdad9de4059d107629e386560549d
Nicolas Lastra <nicolas.lastra.inzunza@gmail.com>
2025-08-04 12:01:08 -0300

    Some routes are defined, but need to be implemented in the project

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,55 @@
 import { Routes } from '@angular/router';
+import { Home } from './home/home';
+import { RoutePaths } from '../shared/routes';
+import { Students } from './features/students/students';
+import { ViewStudent } from './features/students/view-student/view-student';
+import { ViewCourse } from './features/courses/view-course/view-course';
+import { EditStudent } from './features/students/edit-student/edit-student';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    
+    {
+        path: '',
+        component: Students
+    },
+    {
+        path: RoutePaths.STUDENTS,
+        component: Students
+    },
+    {
+        path: RoutePaths.COURSES,
+        loadComponent: () => import('./features/courses/courses').then(m => m.Courses)
+    },
+    {
+        path: RoutePaths.REGISTRATIONS,
+        loadComponent: () => import('./features/registrations/registrations').then(m => m.Registrations)
+    },
+    {
+        path: "view-student",
+        component: ViewStudent
+    },
+    {
+        path: "edit-student",
+        component: EditStudent
+    },
+    {
+        path: "view-course",
+        component: ViewCourse
+    },
+    // {
+    //     path: RoutePaths.HOME,
+    //     component: Home
+    // },
+    // {
+    //     path: RoutePaths.USERS,
+    //     component: User
+    // },
+    // {
+    //     path: "users/:id",
+    //     component: User
+    // }
+    // {
+    //     path: "**",
+    //     component: NotFoundComponent
+    // }
+];
