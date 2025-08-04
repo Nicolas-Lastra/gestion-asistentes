7b70aadd72cd885cd42ccc4742395590c9290b06
Nicolas Lastra <nicolas.lastra.inzunza@gmail.com>
2025-08-04 12:00:30 -0300

    Adding routes to the project to work with router outlet

---
+++ src/shared/routes.ts
@@ -0,0 +1,8 @@
+export enum RoutePaths {
+    HOME = 'home',
+    USERS = 'users',
+    STUDENTS = 'students',
+    COURSES = 'courses',
+    REGISTRATIONS = 'registrations',
+    VIEWSTUDENT = 'view-student',
+}
