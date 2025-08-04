e7bf5f74466ed3bb5c48973474bc3d8053bc7973
Nicolas Lastra <nicolas.lastra.inzunza@gmail.com>
2025-08-04 12:16:37 -0300

    This component can edit student information and is in direct communication with the students-api service.

---
+++ src/app/features/students/edit-student/edit-student.ts
@@ -0,0 +1,85 @@
+import { Component, OnInit} from '@angular/core';
+import { Student } from '../../../../shared/entities';
+import { Router } from '@angular/router';
+import { CommonModule, JsonPipe } from '@angular/common';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatButtonModule } from '@angular/material/button';
+import { StudentsAPI } from '../students-api';
+import { MatSnackBar } from '@angular/material/snack-bar';
+
+@Component({
+  selector: 'app-edit-student',
+  imports: [CommonModule, ReactiveFormsModule, MatInputModule, MatFormFieldModule, MatButtonModule],
+  templateUrl: './edit-student.html',
+  styleUrl: './edit-student.css'
+})
+export class EditStudent implements OnInit {
+
+  editForm!: FormGroup;
+  student!: Student;
+
+  constructor(
+    private router: Router,
+    private fb: FormBuilder,
+    private studentsApi: StudentsAPI,
+    private _snackbar: MatSnackBar
+  ) {
+    const navigation = this.router.getCurrentNavigation();
+    this.student = navigation?.extras.state?.["student"];
+
+    if (!this.student) {
+      this.router.navigate(['/students']);
+    }
+  }
+
+  ngOnInit(): void {
+    this.editForm = this.fb.group({
+      name: [this.student.name, [Validators.required, Validators.minLength(3)]],
+      surname: [this.student.surname, [Validators.required, Validators.minLength(3)]],
+      dni: [this.student.dni, [Validators.required, Validators.min(0)]],
+      email: [this.student.email, [Validators.required, Validators.email]]
+    });
+  }
+
+  onReset() {
+    this.editForm.reset({
+      name: this.student.name,
+      surname: this.student.surname,
+      dni: this.student.dni,
+      email: this.student.email
+    });
+  }
+
+  onSubmit() {
+    if (this.editForm.valid) {
+      const updatedStudent: Student = {
+        ...this.student,
+        ...this.editForm.value
+      };
+
+      console.log('Sending PUT request:', updatedStudent);
+
+      this.studentsApi.editStudent(updatedStudent).subscribe({
+        next: (res) => {
+          this._snackbar.open('Student updated successfully', 'Close', {
+            duration: 3000,
+            panelClass: ['success-snackbar']
+          });
+          this.router.navigate(['/students']);
+        },
+        error: (err) => {
+          this._snackbar.open('Failed to update student', 'Close', {
+            duration: 3000,
+            panelClass: ['error-snackbar']
+          });
+          console.error('Error updating student:', err);
+        }
+      });
+    } else {
+      this.editForm.markAllAsTouched();
+    }
+  }
+
+}
