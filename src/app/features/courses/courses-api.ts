c5f9870c5d7f7843048a1603b0daa0419b314c86
Nicolas Lastra <nicolas.lastra.inzunza@gmail.com>
2025-08-04 14:50:43 -0300

    Adding editing functionality

--- src/app/features/courses/courses-api.ts
+++ src/app/features/courses/courses-api.ts
@@ -7,16 +7,20 @@ import { Course } from '../../../shared/entities';
   providedIn: 'root'
 })
 export class CoursesApi {
-  
+
   baseUrl = "http://localhost:3000";
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) { }
 
   getCourses(): Observable<Course[]> {
     return this.http.get<Course[]>(`${this.baseUrl}/courses`).pipe(delay(1000));
   }
 
   deleteCourse(course: Course): Observable<void> {
-      return this.http.delete<void>(`${this.baseUrl}/courses/${course.id}`).pipe(delay(1000));
-    }
+    return this.http.delete<void>(`${this.baseUrl}/courses/${course.id}`).pipe(delay(1000));
+  }
+
+  editCourse(course: Course): Observable<Course> {
+    return this.http.put<Course>(`${this.baseUrl}/courses/${course.id}`, course).pipe(delay(1000));
+  }
 }
