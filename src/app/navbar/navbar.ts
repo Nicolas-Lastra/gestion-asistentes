386132cfb5e7bcdab85ade499f3f4a56cf5a44d0
Nicolas Lastra <nicolas.lastra.inzunza@gmail.com>
2025-08-04 12:14:41 -0300

    Refactoring the component to work with router-outlet

--- src/app/navbar/navbar.ts
+++ src/app/navbar/navbar.ts
@@ -1,18 +1,13 @@
 import { Component, EventEmitter, Output } from '@angular/core';
 import { NgbNavModule } from '@ng-bootstrap/ng-bootstrap';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
-  imports: [NgbNavModule],
+  imports: [NgbNavModule, RouterModule],
   templateUrl: './navbar.html',
   styleUrl: './navbar.css'
 })
 export class Navbar {
   active = 'studentsList';
-
-  @Output() activeSection = new EventEmitter<string>();
-
-  navigate(section: string) {
-    this.activeSection.emit(section);
-  }
 }
