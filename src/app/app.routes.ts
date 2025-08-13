ee0b46ed61a4d5bf0656d5609044cfc9e515e102
Nicolas Lastra <nicolas.lastra.inzunza@gmail.com>
2025-08-13 20:45:08 -0300

    Application component edited to work with the login page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,43 +6,59 @@ import { ViewCourse } from './features/courses/view-course/view-course';
 import { EditStudent } from './features/students/edit-student/edit-student';
 import { EditCourse } from './features/courses/edit-course/edit-course';
 import { NotFoundComponent } from './not-found-component/not-found-component';
+import { authGuard } from '../shared/guards/auth-guard';
+import { isAdminGuard } from '../shared/guards/is-admin-guard';
+import { Login } from './features/login/login';
 
 export const routes: Routes = [
     {
         path:'',
-        component: Home
+        redirectTo: 'login',
+        pathMatch: 'full'
+    },
+    {
+        path: RoutePaths.LOGIN,
+        component: Login
     },
     {
         path: RoutePaths.HOME,
-        component: Home
+        component: Home,
+        canActivate: [authGuard]
     },
     {
         path: RoutePaths.STUDENTS,
+        canActivate: [authGuard],
         loadComponent: () => import('./features/students/students').then(m => m.Students)
     },
     {
         path: RoutePaths.COURSES,
-        loadComponent: () => import('./features/courses/courses').then(m => m.Courses)
+        loadComponent: () => import('./features/courses/courses').then(m => m.Courses),
+        canActivate: [authGuard]
     },
     {
         path: RoutePaths.REGISTRATIONS,
+        canActivate: [authGuard],
         loadComponent: () => import('./features/registrations/registrations').then(m => m.Registrations)
     },
     {
         path: RoutePaths.VIEWSTUDENT,
-        component: ViewStudent
+        component: ViewStudent,
+        canActivate: [authGuard]
     },
     {
         path: RoutePaths.EDITSTUDENT,
-        component: EditStudent
+        component: EditStudent,
+        canActivate: [authGuard]
     },
     {
         path: RoutePaths.VIEWCOURSE,
-        component: ViewCourse
+        component: ViewCourse,
+        canActivate: [authGuard]
     },
     {
         path: RoutePaths.EDITCOURSE,
-        component: EditCourse
+        component: EditCourse,
+        canActivate: [authGuard]
     },
     {
         path: RoutePaths.DATABASE,
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,19 +1,25 @@
 import { Component } from '@angular/core';
 import { Toolbar } from './toolbar/toolbar';
 import { Navbar } from "./navbar/navbar";
-import { RouterModule } from "@angular/router";
+import { Router, RouterModule } from "@angular/router";
 import { HttpClient } from '@angular/common/http';
 import { MatSnackBar } from '@angular/material/snack-bar';
+import { CommonModule, AsyncPipe } from '@angular/common';
+import { AuthService } from '../shared/services/auth-service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
-  imports: [Toolbar, Navbar, RouterModule],
+  imports: [CommonModule, AsyncPipe, Toolbar, Navbar, RouterModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected title = 'gestion-asistentes';
   activeSection = "studentsSection";
+  isLoggedIn$: Observable<boolean>;
 
-  constructor(private http: HttpClient, private _snackBar: MatSnackBar) { }
+  constructor(private http: HttpClient, private _snackBar: MatSnackBar, private router: Router, private auth: AuthService) {
+    this.isLoggedIn$ = this.auth.isLoggedIn$;
+  }
 }
