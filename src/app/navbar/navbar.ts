cd06ff4500e11c8769f49ae570f8d4664868ef13
Nicolas Lastra <nicolas.lastra.inzunza@gmail.com>
2025-08-14 09:42:51 -0300

    Navbar now shows registrations button only to admin user

--- src/app/navbar/navbar.ts
+++ src/app/navbar/navbar.ts
@@ -1,13 +1,21 @@
 import { Component } from '@angular/core';
 import { NgbNavModule } from '@ng-bootstrap/ng-bootstrap';
 import { RouterModule } from '@angular/router';
+import { AuthService } from '../../shared/services/auth-service';
+import { Observable } from 'rxjs';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'app-navbar',
-  imports: [NgbNavModule, RouterModule],
+  imports: [NgbNavModule, RouterModule, AsyncPipe],
   templateUrl: './navbar.html',
   styleUrl: './navbar.css'
 })
 export class Navbar {
   active = 'homeSection';
+  isAdmin$!: Observable<boolean>;
+  
+  constructor(private auth: AuthService) {
+    this.isAdmin$ = this.auth.isAdmin$;
+  }
 }
