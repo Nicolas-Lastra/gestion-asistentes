16a51fc9cf1b8578d315f47861d299bf697bc1cb
Nicolas Lastra <nicolas.lastra.inzunza@gmail.com>
2025-08-14 10:07:49 -0300

    unnecesary parameters removed

--- src/shared/guards/is-admin-guard.ts
+++ src/shared/guards/is-admin-guard.ts
@@ -2,7 +2,7 @@ import { inject } from '@angular/core';
 import { CanActivateFn, Router } from '@angular/router';
 import { AuthService } from '../services/auth-service';
 
-export const isAdminGuard: CanActivateFn = (route, state) => {
+export const isAdminGuard: CanActivateFn = () => {
 
   const auth = inject(AuthService);
   const router = inject(Router);
