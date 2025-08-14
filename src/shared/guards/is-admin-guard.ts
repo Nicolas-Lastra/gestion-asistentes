b4899beba181cd9d658e1c31d56bb1cdb72d3a3e
Nicolas Lastra <nicolas.lastra.inzunza@gmail.com>
2025-08-14 09:40:31 -0300

    isAdminGuard now verifies user role and redirects to home

--- src/shared/guards/is-admin-guard.ts
+++ src/shared/guards/is-admin-guard.ts
@@ -1,11 +1,16 @@
-import { CanActivateFn } from '@angular/router';
+import { inject } from '@angular/core';
+import { CanActivateFn, Router } from '@angular/router';
+import { AuthService } from '../services/auth-service';
 
 export const isAdminGuard: CanActivateFn = (route, state) => {
 
-  // if (this.user.role === 'admin') {
-  //   return true;
-  // }
+  const auth = inject(AuthService);
+  const router = inject(Router);
 
-  // this.router.navigate(['/not-authorized']);
+  if (auth.isLoggedIn() && auth.isAdmin()) {
+    return true;
+  }
+
+  router.navigate(['/home']);
   return false;
 };
