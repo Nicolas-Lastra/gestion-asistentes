e0bc4b82afd8f0fe41ff509a723f77642109d052
Nicolas Lastra <nicolas.lastra.inzunza@gmail.com>
2025-08-14 17:45:25 -0300

    Should GET students list test added to api connection

--- src/app/features/students/students-api.spec.ts
+++ src/app/features/students/students-api.spec.ts
@@ -1,16 +1,57 @@
-import { TestBed } from '@angular/core/testing';
+import { fakeAsync, TestBed, tick } from '@angular/core/testing';
 
 import { StudentsAPI } from './students-api';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { Student } from '../../../shared/entities';
+import { RoutePaths } from '../../../shared/routes';
 
 describe('StudentsAPI', () => {
   let service: StudentsAPI;
+  let httpMock: HttpTestingController;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [StudentsAPI],
+    });
+
     service = TestBed.inject(StudentsAPI);
+    httpMock = TestBed.inject(HttpTestingController);
+  });
+
+  afterEach(() => {
+    httpMock.verify();
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('should GET students list', fakeAsync(() => {
+
+    // Setup
+
+    const mockStudents: Student[] = [
+      { id: '1', name: 'John', surname: 'Doe', dni: '12345678', email: 'john@example.com' },
+      { id: '2', name: 'Jane', surname: 'Smith', dni: '87654321', email: 'jane@example.com' },
+    ];
+
+    let result: Student[] | undefined;
+
+    // Act
+    service.getStudents().subscribe(res => (result = res));
+    
+    const req = httpMock.expectOne(`${service.baseUrl}/${RoutePaths.STUDENTS}`);
+    expect(req.request.method).toBe('GET');
+    req.flush(mockStudents);
+
+    tick(1000);
+
+    // Assert
+    expect(result).toBeTruthy();
+    expect(result!.length).toBe(2);
+    expect(result![0].name).toBe('John');
+    expect(result![1].email).toBe('jane@example.com');
+  }));
+
 });
