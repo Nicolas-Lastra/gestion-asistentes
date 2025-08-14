69d63a080a3c5b2e89098c4ff68d2f76689aeecd
Nicolas Lastra <nicolas.lastra.inzunza@gmail.com>
2025-08-14 15:04:24 -0300

    Added logout functionality

--- src/app/toolbar/toolbar.ts
+++ src/app/toolbar/toolbar.ts
@@ -1,11 +1,28 @@
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
+import { User } from '../../shared/entities';
+import { AuthService } from '../../shared/services/auth-service';
+import { Router } from '@angular/router';
+import { RoutePaths } from '../../shared/routes';
+import { AsyncPipe, CommonModule } from '@angular/common';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-toolbar',
-  imports: [],
+  imports: [CommonModule, AsyncPipe, MatButtonModule],
   templateUrl: './toolbar.html',
   styleUrl: './toolbar.css'
 })
 export class Toolbar {
 
+  user$!: Observable<User | null>;
+
+  constructor(private auth: AuthService, private router: Router) {
+    this.user$ = this.auth.user$;
+  }
+
+  logout(): void {
+    this.auth.logout();
+    this.router.navigate([RoutePaths.LOGIN]);
+  }
 }
