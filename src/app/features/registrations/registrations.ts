dcad4ee5c6626314dd8c0eca6e5922e5a912a3f7
Nicolas Lastra <nicolas.lastra.inzunza@gmail.com>
2025-08-21 14:42:28 -0300

    Registrations component updated to work with student and course creation options

--- src/app/features/registrations/registrations.ts
+++ src/app/features/registrations/registrations.ts
@@ -1,11 +1,21 @@
-import { Component } from '@angular/core';
+import { NgIf } from '@angular/common';
+import { Component, signal } from '@angular/core';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
+import { MatTabsModule } from '@angular/material/tabs';
+import { CreateStudentForm } from './create-student-form/create-student-form';
+import { CreateCourseForm } from './create-course-form/create-course-form';
 
 @Component({
   selector: 'app-registrations',
-  imports: [],
+  imports: [NgIf, MatTabsModule, MatButtonToggleModule, CreateStudentForm, CreateCourseForm],
   templateUrl: './registrations.html',
   styleUrl: './registrations.css'
 })
 export class Registrations {
 
+  view = signal<'student' | 'course'>('student');
+
+  onTabChange(idx: number) {
+    this.view.set(idx === 0 ? 'student' : 'course');
+  }
 }
