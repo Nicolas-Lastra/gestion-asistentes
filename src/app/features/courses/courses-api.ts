0188507373dc53f6fad47edf74216d631ff5d67c
Nicolas Lastra <nicolas.lastra.inzunza@gmail.com>
2025-08-22 10:31:57 -0300

    Hardcoded url updated with RoutePaths

--- src/app/features/courses/courses-api.ts
+++ src/app/features/courses/courses-api.ts
@@ -14,15 +14,15 @@ export class CoursesApi {
   constructor(private http: HttpClient) { }
 
   getCourses(): Observable<Course[]> {
-    return this.http.get<Course[]>(`${this.baseUrl}/courses`).pipe(delay(1000));
+    return this.http.get<Course[]>(`${this.baseUrl}/${RoutePaths.COURSES}`).pipe(delay(1000));
   }
 
   deleteCourse(course: Course): Observable<void> {
-    return this.http.delete<void>(`${this.baseUrl}/courses/${course.id}`).pipe(delay(1000));
+    return this.http.delete<void>(`${this.baseUrl}/${RoutePaths.COURSES}/${course.id}`).pipe(delay(1000));
   }
 
   editCourse(course: Course): Observable<Course> {
-    return this.http.put<Course>(`${this.baseUrl}/courses/${course.id}`, course).pipe(delay(1000));
+    return this.http.put<Course>(`${this.baseUrl}/${RoutePaths.COURSES}/${course.id}`, course).pipe(delay(1000));
   }
 
   createCourse(payload: Omit<Course, 'id'>): Observable<Course> {
